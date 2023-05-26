4424c00cf26ca871eb1080d5524e065ab2d3c916
niushayousefi <niusha.mech@gmail.com>
2023-05-26 20:26:40 +0300

    add fake data to query

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const { buildSchema } = require('graphql')
+const { graphqlHTTP } = require('express-graphql')
+
+const PORT = 4000;
+
+const schema = buildSchema(`
+type Query {
+    products: [Product]
+    orders: [Order]
+  }
+  type Product {
+    id: ID!
+    description: String!
+    reviews: [Review]
+    price: Float!
+  }
+  type Review {
+    rating: Int!
+    comment: String
+  }
+  type Order {
+    date: String!
+    subtotal: Float!
+    items: [OrderItem]
+  }
+  type OrderItem {
+    product: Product!
+    quantity: Int!
+  }
+`);
+
+const root = {
+    products: [
+        {
+          id: 'redshoe',
+          description: 'Red Shoe',
+          price: 42.12,
+        },
+        {
+          id: 'bluejean',
+          description: 'Blue Jeans',
+          price: 55.55,
+        }
+      ],
+      orders: [
+        {
+          date: '2005-05-05',
+          subtotal: 90.22,
+          items: [ 
+            {
+              product: {
+                id: 'redshoe',
+                description: 'Old Red Shoe',
+                price: 45.11,
+              },
+              quantity: 2,
+            }
+          ]
+        }
+      ]
+}
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+app.listen(PORT, () => {
+    console.log(`Running GraphQL server...`);
+})
